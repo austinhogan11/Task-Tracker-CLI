cbdb6ec9b468db39ad7c6ae0ce25d6e9b19ff960
Austin Hogan <austinhogandev@gmail.com>
2024-10-09 13:32:46 -0400

    Add options menu and take user input.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+#TODO 1: Present User with options & Request User Input
 print("Welcome to Task Tracker 1.0")
 
-#TODO 1: Present User with options & Request User Input
+trackerIsRunning = True
+
+while trackerIsRunning:
+    user_selection = int(input("Please select an option:\n1. List Tasks\n2. Add Task\n3. Update Task\n4. Delete Task\n5. Exit\n"))
+
+    if user_selection == 1:
+        print("List Tasks")
+    elif user_selection == 2:
+        print("Add Task")
+    elif user_selection == 3:
+        print("Update Task")
+    elif user_selection == 4:
+        print("Delete Task")
+    elif user_selection == 5:
+        print("Exiting Tracker")
+        trackerIsRunning = False
+    else:
+        print("Invalid Option")
 
 #TODO 2: Handle User Selection
 
