5ff3704e077262c4d26506efe0e2f5cec98f10f3
Austin Hogan <austinhogandev@gmail.com>
2024-10-09 13:24:05 -0400

    Task Tracker CLI Initialization

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome to Task Tracker 1.0")
+
+#TODO 1: Present User with options & Request User Input
+
+#TODO 2: Handle User Selection
+
+#TODO 3: List all tasks, done, not done, in progress
+
+#TODO 4: Add Tasks to Tracker
+
+#TODO 5: Update Tasks Progress
+
+#TODO 6: Delete Task from Tracker
+
+#TODO 7 Store tasks in json file
